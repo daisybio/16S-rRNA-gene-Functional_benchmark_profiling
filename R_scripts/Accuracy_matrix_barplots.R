e0979adfab840ead9b07775f858d4863cb6f0b59
Monica Steffi <12621929+MonicaSteffi@users.noreply.github.com>
2023-10-26 13:37:21 +0200

    Update Accuracy_matrix_barplots.R

--- R_scripts/Accuracy_matrix_barplots.R
+++ R_scripts/Accuracy_matrix_barplots.R
@@ -3,6 +3,40 @@ library(ggplot2)
 library(stringr)
 library(reshape2)
 
+##########Figure 4: Comparison of detected KEGG terms between inferred metagenomes and MGS#############
+
+
+accuracy_quantitative<-read.table("../dataset/accuracy_matrix_quantitative.tsv",sep="\t",header=TRUE)
+accuracy_quantitative<-accuracy_quantitative[,c(1,7:9,11)]
+
+accuracy_q<-reshape2::melt(accuracy_quantitative)
+colnames(accuracy_q)<-c("Method","Cohort","Metrices","Value")
+accuracy_q<-dplyr::filter(accuracy_q,Cohort!="IBD")
+
+pdf("F:/Cohort/github/Figures/Figure_4_Quantitative_barplt.pdf",width=120, height=50)
+ggplot(accuracy_q,aes(x=Method, y=Value, fill=Method)) +
+  geom_bar(stat="identity") +
+  facet_grid(Metrices~Cohort, scales = "free", space = "free", switch="y") +
+  theme(panel.grid.major.x = element_blank(),
+        axis.line = element_line(colour = "black"),axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.x=element_text(angle=45, hjust=1,size=15,face="bold"),
+        legend.position = "none",strip.background =element_rect(fill="#FDEEDC"),
+        panel.spacing.x=unit(0.1, "lines") , panel.spacing.y=unit(2,"lines"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        strip.text = element_text(size=18, face="bold"),
+        legend.text=element_text(size=18,face="bold"),
+        legend.key.size = unit(1,"line"), 
+        legend.title=element_text(size=18, face="bold"),
+        axis.text.y = element_text(size=15,face="bold"))+
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 1))+
+  scale_fill_manual(values = c("#554994","#FF8DC7","#F29393","#FFCCB3"))
+dev.off()
+
+
+##############Figure 6: Comparison of significantly differentially abundant KO terms between inferred metagenomes and MGS
+
 file_list <- list.files(c("F:/Cohort/github/dataset/"),pattern = "differential_abundance_accuracy_matrix_*",full.names = TRUE)
 print(file_list)
 accuracy <- do.call("rbind", lapply(file_list, FUN = function(file) {
